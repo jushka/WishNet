a72a419380cf39072d41f6cf82d7cd22d74ef077
Tautrimas <tautrimas@inbox.lt>
2019-07-23 12:47:15 +0000

    session and passport config

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express"),
-	  app = express(),
-	  port = process.env.PORT || 3000;
+	  session = require("express-session"),
+	  passport = require("passport");
+
+const app = express();
 
 // requiring routes
 const indexRoutes = require("./routes/index.js");
 
 app.set("view engine", "ejs");
 
+// session and passport configuration
+app.use(session({
+	secret: "What is your biggest desire?",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
 app.use("/", indexRoutes);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log("WishNet server has started!");
