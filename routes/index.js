064c7fc90c16947dc6aabe3b4ff0895d845c16b9
Tautrimas <tautrimas@inbox.lt>
2019-07-23 11:16:52 +0000

    create log in, sign up, logout routes and views

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 const express = require("express"),
 	  router = express.Router();
 
+// root
 router.get("/", (req, res) => {
 	res.render("index");
+});
+
+// show sign up form
+router.get("/signup", (req, res) => {
+	res.render("signup");
+});
+
+// handle sign up
+
+// show log in form
+router.get("/login", (req, res) => {
+	res.render("login");
+});
+
+// handle log in
+
+// handle log out
+router.get("/logout", (req, res) => {
+	res.redirect("/");
 });
 
 module.exports = router;
