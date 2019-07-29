71b35dbc0edf5e622a5cfee8fc50b03591f3f5ce
Tautrimas <tautrimas@inbox.lt>
2019-07-29 19:28:11 +0000

    final fix indentation

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,26 @@ const express = require("express"),
 
 // root
 router.get("/", (req, res) => {
-	res.render("index");
+  res.render("index");
 });
 
 // show sign up form
 router.get("/signup", (req, res) => {
-	res.render("signup");
+  res.render("signup");
 });
 
 // handle sign up
 
 // show log in form
 router.get("/login", (req, res) => {
-	res.render("login");
+  res.render("login");
 });
 
 // handle log in
 
 // handle log out
 router.get("/logout", (req, res) => {
-	res.redirect("/");
+  res.redirect("/");
 });
 
 module.exports = router;
