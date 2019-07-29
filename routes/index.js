09ce5769d74efe37dac9fa6e2f909b3ac1ece555
Tautrimas <tautrimas@inbox.lt>
2019-07-29 20:29:39 +0000

    add signup, login, logout routes logic

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require("express"),
-      router = express.Router();
+      passport = require("passport"),
+      bcrypt = require("bcryptjs"),
+      User = require("../models/user"),
+      router = express.Router(),
+      forwardLoggedIn = require("../middleware/auth").forwardLoggedIn;
 
 // root
 router.get("/", (req, res) => {
@@ -7,21 +11,89 @@ router.get("/", (req, res) => {
 });
 
 // show sign up form
-router.get("/signup", (req, res) => {
+router.get("/signup", forwardLoggedIn, (req, res) => {
   res.render("signup");
 });
 
 // handle sign up
+router.post("/signup", (req, res) => {
+  const {username, email, password, password2} = req.body;
+  let errors = [];
+  
+  if(!username || !email || !password || !password2) {
+    errors.push({message: "Please enter all fields"});
+  }
+  
+  if(password != password2) {
+    errors.push({message: "Passwords do not match"});
+  }
+  
+  if(password.length < 6) {
+    errors.push({message: "Password must be at least 6 characters long"});
+  }
+  
+  if(errors.length > 0) {
+    res.render("signup", {
+      errors,
+      username,
+      email,
+      password,
+      password2
+    });
+  } else {
+    User.findOne({email: email}).then((user) => {
+      if(user) {
+        errors.push({message: "User with such email already exists"});
+        res.render("signup", {
+          errors,
+          username,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({
+          username,
+          email,
+          password
+        });
+        
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) {
+              throw err;
+            }
+            newUser.password = hash;
+            newUser.save().then((user) => {
+              req.flash("success", "Your account has been created. You can log in now");
+              res.redirect("login");
+            }).catch((err) => {
+              console.log(err);
+            });
+          });
+        });
+      }
+    });
+  }
+});
 
 // show log in form
-router.get("/login", (req, res) => {
+router.get("/login", forwardLoggedIn, (req, res) => {
   res.render("login");
 });
 
 // handle log in
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true
+}), (req, res) => {
+});
 
 // handle log out
 router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success", "You are logged out now");
   res.redirect("/");
 });
 
