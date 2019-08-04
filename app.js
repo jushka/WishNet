97c616013f8c43de5c3125f9d7124a7198b093ba
Tautrimas <tautrimas@inbox.lt>
2019-08-04 18:05:50 +0000

    add signup username check

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ mongoose.connect(mongoURL, {
 
 // app config
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(flash());
 
---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+// Restricts input for the given textbox to the given inputFilter.
+function setInputFilter(textbox, inputFilter) {
+  ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+    textbox.addEventListener(event, function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      }
+    });
+  });
+}
+
+// Restrict input by using a regular expression filter.
+setInputFilter(document.getElementById("username"), function(value) {
+  console.log(value);
+  return /^[a-z0-9._]*$/.test(value);
+});
