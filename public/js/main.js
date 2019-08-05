8c6de5b79b71e303db793d35ff413c47979c04c0
Tautrimas <tautrimas@inbox.lt>
2019-08-05 16:17:21 +0000

    minor fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-// Restricts input for the given textbox to the given inputFilter.
+// Restricts input for the given textbox to the given inputFilter
 function setInputFilter(textbox, inputFilter) {
   ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
     textbox.addEventListener(event, function() {
@@ -14,18 +14,20 @@ function setInputFilter(textbox, inputFilter) {
   });
 }
 
-// Restrict input by using a regular expression filter.
-setInputFilter(document.getElementById("username"), function(value) {
-  return /^[a-z0-9._]*$/.test(value);
-});
-
-// Constantly checks entered username
-["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
-  document.getElementById("username").addEventListener(event, function() {
-    if (!/^[a-z0-9]+[a-z0-9._][a-z0-9]+$/.test(this.value) || this.value.length < 6 || this.value.length > 30) {
-      this.style.borderBottom = "2px solid red";
-    } else {
-      this.style.borderBottom = "";
-    }
+if(document.getElementById("username") != null) {
+  // Restrict input by using a regular expression filter
+  setInputFilter(document.getElementById("username"), function(value) {
+    return /^[a-z0-9._]*$/.test(value);
   });
-});
+  
+  // Constantly checks entered username
+  ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+    document.getElementById("username").addEventListener(event, function() {
+      if (!/^[a-z0-9]+[a-z0-9._][a-z0-9]+$/.test(this.value) || this.value.length < 6 || this.value.length > 30) {
+        this.style.borderBottom = "2px solid red";
+      } else {
+        this.style.borderBottom = "";
+      }
+    });
+  });
+}
